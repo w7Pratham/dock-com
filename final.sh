57bcc7a407e4098a0e16ed98539e3d28610d3cb7
w7Pratham <prathamshingne@gmail.com>
2023-11-05 13:03:15 +0530

    first

---
+++ final.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker compose up -d && \
+docker cp ./alert_rules.yml proms-1:/root/proms/alert_rules.yml && \
+docker cp ./prometheus.yml proms-1:/root/proms/prometheus.yml && \
+docker cp ./alertmanager.yml proms-1:/root/alerts/alertmanager.yml && \
+docker exec proms-1 sh -c "/root/proms/prometheus --config.file=/root/proms/prometheus.yml" &&\
+docker exec proms-1 sh -c "/root/alerts/alertmanager --config.file=/root/alerts/alertmanager.yml"
